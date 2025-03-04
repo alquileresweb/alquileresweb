b8f2c48022afda13cbf34acde427ce1b1e55f9e4
alquileresweb <alquileresweb1@gmail.com>
2025-03-04 18:31:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz-f1Wzh7g21CvvVfrYYRGs9Gm2wDJaJ8Ro00AGkpA2aprBlyoQ_kv1t8pXFAvaUL7J/exec";
+const GOOGLE_SCRIPT_URL = https://script.google.com/macros/s/AKfycbz-f1Wzh7g21CvvVfrYYRGs9Gm2wDJaJ8Ro00AGkpA2aprBlyoQ_kv1t8pXFAvaUL7J/exec;
