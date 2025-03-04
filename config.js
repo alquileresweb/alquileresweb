bc7164309515734c232063be15b0cb859e7d28c8
alquileresweb <alquileresweb1@gmail.com>
2025-03-04 19:02:42 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-const GOOGLE_SCRIPT_URL = https://script.google.com/macros/s/AKfycbz-f1Wzh7g21CvvVfrYYRGs9Gm2wDJaJ8Ro00AGkpA2aprBlyoQ_kv1t8pXFAvaUL7J/exec;
+// Aquí va tu clave de API de Google Maps
+const GOOGLE_MAPS_API_KEY = "AIzaSyCXPX52c062dXYvJBITx6gjd2qDcbvi_G0";
+
+// Configuración del ID de la hoja de cálculo de Google Sheets
+const SPREADSHEET_ID = "1UaLlghoKp5eF8bonlWGV6EqWg-3K7iee2wG2raneoOI";
+
+// Aquí puedes agregar otras configuraciones si es necesario, por ejemplo, URLs de APIs, etc.
