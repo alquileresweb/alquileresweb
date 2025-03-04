e67b3bd3add7c163f3496b61cd8df27638f6b0bf
alquileresweb <alquileresweb1@gmail.com>
2025-03-04 19:12:29 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
-// Asegúrate de que este código esté bien escrito
-const GOOGLE_MAPS_API_KEY = "AIzaSyCXPX52c062dXYvJBITx6gjd2qDcbvi_G0"; // Reemplaza con tu clave API de Google Maps
-
-// URL de Google Apps Script (reemplaza por tu URL específica)
+// config.js
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxmoiEnvG-x-Ium5QBAuADIPYI_rIy5Y-azdHDQnpmczlEWSTkHNxRge2VvXxR0MR2x/exec";
