bbf13fecd2b9a5d7fd7425218dfd4ec744e7a42d
alquileresweb <alquileresweb1@gmail.com>
2025-03-04 21:37:20 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-
-// Definir la URL de Google Apps Script
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxmoiEnvG-x-Ium5QBAuADIPYI_rIy5Y-azdHDQnpmczlEWSTkHNxRge2VvXxR0MR2x/exec";
