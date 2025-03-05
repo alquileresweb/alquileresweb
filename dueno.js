1ee8a21749013cec385b9dc1e167cb26ffc3bff8
alquileresweb <alquileresweb1@gmail.com>
2025-03-04 21:48:53 -0300

    Create dueno.js

---
+++ dueno.js
@@ -0,0 +1,7 @@
+document.getElementById('nuevoAlquiler').addEventListener('click', function() {
+  window.location.href = '/formularioNuevoAlquiler.html';  // Redirigir al formulario para agregar alquiler
+});
+
+document.getElementById('misAlquileres').addEventListener('click', function() {
+  window.location.href = '/misAlquileres.html';  // Redirigir a la página de mis alquileres
+});
